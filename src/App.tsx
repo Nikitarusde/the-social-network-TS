37aa5572ab9448de817d297cc5bbd2d4199182d8
Nikita <nikistosov@gmail.com>
2021-12-28 20:44:37 +0100

    step11

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
+import "./logo.jpg"
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={"app-wrapper"}>
+            <header className={"header"}>
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Emoji_u1f335.svg/768px-Emoji_u1f335.svg.png"
+                    alt="#"/>
+            </header>
+            <nav className={"nav"}>
+                <div>
+                    <a href="">Profile</a>
+                </div>
+                <div>
+                    <a href="">Messages</a>
+                </div>
+                <div>
+                    <a href="">News</a>
+                </div>
+                <div>
+                    <a href="">Music</a>
+                </div>
+                <div>
+                    <a href="">Settings</a>
+                </div>
+            </nav>
+            <div className={"content"}>
+                <div>
+                    <img
+                        src="https://images.unsplash.com/photo-1549262166-10bfa568ae30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+                        alt=""/>
+                </div>
+                <div>
+                    ava
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/8/87/Avatar_poe84it.png" alt=""/>
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>
+                            post1
+                        </div>
+                        <div>
+                            post2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
+
 
 export default App;
